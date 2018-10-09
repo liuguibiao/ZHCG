880247b157a570e7e68877b3ba230f0ed7c5d648
lgb123 <1361301494@qq.com>
2018-10-09 22:07:45 +0800

    IdentityServer4第一版

---
+++ Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
+        {
+            // 从元数据中发现客户端
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+
+            // 请求令牌
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "ro.client", "secret");
+            var tokenResponse = await tokenClient.RequestResourceOwnerPasswordAsync("alice", "password", "api1");//使用用户名密码
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+        }
+    }
+}
